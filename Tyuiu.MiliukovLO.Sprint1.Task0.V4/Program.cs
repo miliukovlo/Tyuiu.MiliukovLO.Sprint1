b65c86fa0ed356474f527a323556186b68bb9e32
ssadown <bibatututu@yandex.ru>
2024-09-17 09:12:42 +0500

    Task0

--- Tyuiu.MiliukovLO.Sprint1.Task0.V4/Program.cs
+++ Tyuiu.MiliukovLO.Sprint1.Task0.V4/Program.cs
@@ -4,6 +4,27 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DataService dataService = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Милюков Л. О. | ИИПб 24-2";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                                ");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                         ");
+        Console.WriteLine("* Задание #0                                                               ");
+        Console.WriteLine("* Вариант #4                                                               ");
+        Console.WriteLine("* Выполнил: Милюков Леонид Олегович | ИИПб 24-2                            ");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                 ");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение 4/2*5/(3+2)*(5-2)        ");
+        Console.WriteLine("* и печатает результат на экране.                                          ");
+        Console.WriteLine("*                                                                          ");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* 4/2*5/(3+2)*(5-2)                                                        ");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+        Console.WriteLine("***************************************************************************");
+        dataService.Calculate();
+        Console.ReadKey();
     }
 }
